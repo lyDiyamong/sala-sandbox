1f80cc07baf2a2b53f73cd390b74c06735fd86a1
Thavorac <thavorac@sala.co>
2025-03-11 10:49:54 +0700

    Set up VueJs with Antd Template

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <a-button type="primary">Ant Design Vue Button</a-button>
+</template>
+
+<script setup></script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
+import Alpine from "alpinejs";
 
-import Alpine from 'alpinejs';
+import { createApp } from "vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+import App from "./App.vue";
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+createApp(App).use(Antd).mount("#app");
