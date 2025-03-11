beddf2bed80546af52e9675cfc071a7c293f7ac8
lyDiyamong <diyamong26@gmail.com>
2025-03-11 14:45:57 +0700

    feat: config typescript for vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,10 @@
 <template>
     <a-button type="primary">Ant Design Vue Button</a-button>
+    <div class="text-red-500">Hello World</div>
+    <MyButton label="Click me" />
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import MyButton from "@/components/MyButton.vue";
+</script>
+
