bb4bc7da3bbc25c6044ce1ddfc034c2e07226b59
lyDiyamong <diyamong26@gmail.com>
2025-03-12 10:43:14 +0700

    fix: tsc error

---
+++ resources/js/components/MyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button class="bg-blue-500 text-white p-2 rounded">{{ props.label }}</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ label: string }>()
+</script>
+
+<style>
+
+</style>
